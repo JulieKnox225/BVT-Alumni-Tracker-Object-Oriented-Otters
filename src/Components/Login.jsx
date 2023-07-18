8bce3096b1a4141f88956d126e569acffcb2a878
Andre <andre.rodriguez.cohort229@gmail.com>
2023-07-18 15:30:13 -0700

    small changes to css

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -116,7 +116,7 @@ export const Login = () => {
                 </div>
 
                 <div>
-                  <p className="login-text">Don&apos;t have an account? <Link to = '/addEntryPage' className="sign-up-login">Sign Up</Link></p>
+                  <p className="login-text">Don&apos;t have an account? <Link to = '/register' className="sign-up-login">Sign Up</Link></p>
                 <p><Link  to = '/forgotPassword' className="fp-login">Forgot password?</Link></p>
                 </div>
               </MDBCardBody>
