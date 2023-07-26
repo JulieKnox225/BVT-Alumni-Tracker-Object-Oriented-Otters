88179493a582d0d5539ddf435006b251625bfc5c
JoMiras <jonah@bayvalleytech.com>
2023-07-26 02:54:58 -0700

    Middleware + app.js + route

---
+++ server/routes/api/alumniRoutes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const alumniController = require('../controllers/alumniController');
+
+// Alumni search endpoint
+router.get('/search', alumniController.searchAlumni);
+router.get('/', alumniController.getAllAlumni);
+
+// Other alumni-related endpoints
+router.post('/', alumniController.createAlumni);
+router.put('/', alumniController.updateAlumni);
+router.put('/profile', alumniController.editProfile);
+router.get('/:id', alumniController.getAlumniById);
+router.get('/name/:name', alumniController.getAlumniByName);
+router.get('/year/:year', alumniController.getAlumniByYear);
+
+module.exports = router;
