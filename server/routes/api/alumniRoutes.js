e7c7dc1923aa7e766db32b01c1a62b8af337cf4e
JoMiras <jonah@bayvalleytech.com>
2023-08-01 00:25:19 -0700

    Modifying alumniRoutes and renaming alumniController.js

--- server/routes/api/alumniRoutes.js
+++ server/routes/api/alumniRoutes.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path');
 const alumniController = require('../controllers/alumniController');
+
 
 // Alumni search endpoint
 router.get('/search', alumniController.searchAlumni);
