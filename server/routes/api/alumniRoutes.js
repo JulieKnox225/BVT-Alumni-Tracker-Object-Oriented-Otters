41f295ea2ac0bf9f983ef7dcb5fd9a73e6abc681
JoMiras <jonah@bayvalleytech.com>
2023-08-01 00:49:36 -0700

    Fixed error within line 4 in order to find the module.

--- server/routes/api/alumniRoutes.js
+++ server/routes/api/alumniRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
-const alumniController = require('../controllers/alumniController');
+const alumniController = require('../../controllers/alumniController');
 
 
 // Alumni search endpoint
